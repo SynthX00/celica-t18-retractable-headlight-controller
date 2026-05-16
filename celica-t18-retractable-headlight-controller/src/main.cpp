2eae5945f52c2d435c18bc0bc88f17501e078a9f
daniel <17280945+SynthX00@users.noreply.github.com>
2026-05-16 21:14:21 +0100

    testing motor mvmt sensors

--- celica-t18-retractable-headlight-controller/src/main.cpp
+++ celica-t18-retractable-headlight-controller/src/main.cpp
@@ -1,19 +1,24 @@
 #include <Arduino.h>
-
 void setup() {
   Serial.begin(115200);
-
-  pinMode(4, INPUT_PULLUP);
-  pinMode(6, INPUT_PULLUP);
-  pinMode(10, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
+  
+  // Set Pin 9 to output 5V and listen for the optocoupler to pull it to Ground
+  pinMode(9, INPUT_PULLUP); 
+  
+  Serial.println("--- Right Headlight Sensor Test (Pin 7) ---");
+  Serial.println("Turn the manual knob on the right motor to see the state change.");
 }
 
 void loop() {
-  Serial.print(digitalRead(4));
-  Serial.print(digitalRead(6));
-  Serial.print(digitalRead(10));
-  Serial.println(digitalRead(12));
+  int sensorState = digitalRead(9);
 
-  delay(20);
+  if (sensorState == LOW) { 
+    // Optocoupler LED is ON because Pin 7 is spitting 12V
+    Serial.println("Signal: 0  |  State: MOTOR MOVING (Wiper is on the copper)");
+  } else { 
+    // Optocoupler LED is OFF because Pin 7 is dead
+    Serial.println("Signal: 1  |  State: FULLY PARKED UP (Wiper is on the plastic)");
+  }
+
+  delay(100); 
 }
